b2c0ea43cad6813d7d10f5f1851f51cf0629904c
Nguyen Tuan Anh <xiaoma@Nguyens-MacBook-Pro.local>
2020-02-03 18:36:58 +0700

    add

--- src/Layout.elm
+++ src/Layout.elm
@@ -1,6 +1,24 @@
 module Layout exposing (view)
 
-import Element exposing (..)
+import Element
+    exposing
+        ( Element
+        , alpha
+        , centerX
+        , column
+        , el
+        , fill
+        , height
+        , link
+        , maximum
+        , mouseOver
+        , paddingEach
+        , rgb255
+        , row
+        , spacing
+        , text
+        , width
+        )
 import Element.Font as Font
 import Generated.Routes as Routes exposing (Route, routes)
 import Utils.Spa as Spa
@@ -31,19 +49,15 @@ viewLink : Route -> ( String, Route ) -> Element msg
 viewLink currentRoute ( label, route ) =
     if currentRoute == route then
         el
-            [ Font.underline
-            , Font.color (rgb255 204 75 75)
-            , alpha 0.5
-            , Font.size 16
+            [ Font.color (rgb255 97 96 97)
+            , Font.size 15
             ]
             (text label)
 
     else
         link
-            [ Font.underline
-            , Font.color (rgb255 204 75 75)
-            , Font.size 16
-            , mouseOver [ alpha 0.5 ]
+            [ Font.color (rgb255 128 127 128)
+            , Font.size 15
             ]
             { label = text label
             , url = Routes.toPath route
